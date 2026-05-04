c9a1095d2c9a977ffe7349d748acf8a8f4f165a8
GabrielVanderlinde <gabrielvanderlindezz@gmail.com>
2026-05-04 19:29:50 -0300

    feat: cria estrutura de paciente entity

--- src/main/java/com/senai/clinicaApi/entites/PacienteEntity.java
+++ src/main/java/com/senai/clinicaApi/entites/PacienteEntity.java
@@ -1,4 +1,47 @@
 package com.senai.clinicaApi.entites;
 
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "paciente")
 public class PacienteEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private Long id;
+    @Column(name = "nome", nullable = false)
+    private String nome;
+    @Column(name = "email", nullable = false, unique = true)
+    private String email;
+
+    //Constructor
+
+    public PacienteEntity() {
+    }
+
+    //Get and Set
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
 }
