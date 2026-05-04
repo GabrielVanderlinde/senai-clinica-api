037c4ee0adba1bae3fff28dc2394feb8b9637494
GabrielVanderlinde <gabrielvanderlindezz@gmail.com>
2026-05-04 20:16:42 -0300

    feat: cria paciente repository

---
+++ src/main/java/com/senai/clinicaApi/repositories/PacienteRepository.java
@@ -0,0 +1,13 @@
+package com.senai.clinicaApi.repositories;
+
+import com.senai.clinicaApi.entites.PacienteEntity;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface PacienteRepository extends JpaRepository<PacienteEntity, Long> {
+
+    Optional<PacienteEntity> findByEmail(String email);
+
+    boolean existsByEmail(String email);
+}
