d17de5f924e39b7e20dd8a1e3489418df61effd2
GabrielVanderlinde <gabrielvanderlindezz@gmail.com>
2026-05-05 14:21:20 -0300

    refactor: corrige imports entities

--- src/main/java/com/senai/clinicaApi/controllers/PacienteController.java
+++ src/main/java/com/senai/clinicaApi/controllers/PacienteController.java
@@ -1,7 +1,7 @@
 package com.senai.clinicaApi.controllers;
 
 import com.senai.clinicaApi.dtos.PacienteDto;
-import com.senai.clinicaApi.entites.PacienteEntity;
+import com.senai.clinicaApi.entities.PacienteEntity;
 import com.senai.clinicaApi.services.PacienteService;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
