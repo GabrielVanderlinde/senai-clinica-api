9603e9c4c42d1051f1fd7c7f3a8ab2ae105630d2
GustavEduardoCerebelDeLima <ozzybombado@gmail.com>
2026-05-05 13:58:26 -0300

    inserido Paciente

---
+++ src/main/java/com/senai/clinicaApi/controllers/PacienteController.java
@@ -0,0 +1,60 @@
+package com.senai.clinicaApi.controllers;
+
+import com.senai.clinicaApi.dtos.PacienteDto;
+import com.senai.clinicaApi.entities.PacienteEntity;
+import com.senai.clinicaApi.services.PacienteService;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/paciente")
+public class PacienteController {
+
+    //Injeção de Dependências
+    private PacienteService service;
+
+    public PacienteController(PacienteService service) {
+    }
+
+    //Methods
+    @PostMapping("/paciente")
+    public ResponseEntity<Boolean> cadastrarPaciente(@RequestBody PacienteDto pacienteDto) {
+        Boolean resposta = service.inserirPaciente(pacienteDto);
+
+        if (!resposta) {
+            return ResponseEntity.badRequest().body(resposta);
+        }
+
+        return ResponseEntity.ok().body(resposta);
+    }
+
+    @GetMapping("/pacientes")
+    public ResponseEntity<List<PacienteDto>> listarPacientes() {
+        List<PacienteDto> pacientes = service.obterPacientes();
+        return ResponseEntity.ok(pacientes);
+    }
+
+    @GetMapping("paciente/{email}")
+    public ResponseEntity<String> obterPacientePorEmail(@PathVariable String email) {
+        PacienteEntity paciente = service.obterPaciente(email);
+
+        if (paciente == null) {
+            return ResponseEntity.status(400).body("Paciente com email :" + email + " não encontrado.");
+        }
+        return null;
+    }
+
+    @PutMapping("/paciente/{email}")
+    public ResponseEntity<Boolean> atualizarPaciente(@PathVariable String email, @RequestBody PacienteDto pacienteDto) {
+        Boolean resposta = service.atualizarPaciente(email, pacienteDto);
+
+        if (!resposta) {
+            return ResponseEntity.badRequest().body(resposta);
+        }
+        return ResponseEntity.ok().body(resposta);
+    }
+
+    //Delete pendente (aguardando Gustavo)
+}
