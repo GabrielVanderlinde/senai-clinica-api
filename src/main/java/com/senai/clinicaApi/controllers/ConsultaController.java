de665ca7244ae72d8e3c52fadce124559935176a
GabrielVanderlinde <gabrielvanderlindezz@gmail.com>
2026-05-06 15:00:54 -0300

    fix: corrige estruturas de validacoes e enderecamento de requisicoes

--- src/main/java/com/senai/clinicaApi/controllers/ConsultaController.java
+++ src/main/java/com/senai/clinicaApi/controllers/ConsultaController.java
@@ -1,56 +1,50 @@
 package com.senai.clinicaApi.controllers;
 
 import com.senai.clinicaApi.dtos.ConsultaDto;
-import com.senai.clinicaApi.dtos.PacienteDto;
-import com.senai.clinicaApi.entities.PacienteEntity;
-import com.senai.clinicaApi.repositories.ConsultaRepository;
 import com.senai.clinicaApi.services.ConsultaService;
-import com.senai.clinicaApi.services.PacienteService;
-import org.apache.coyote.Response;
+import jakarta.validation.Valid;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
-import java.util.Optional;
+
 
 @RestController
 @RequestMapping("/consulta")
 public class ConsultaController {
     private ConsultaService service;
 
-    public ConsultaController(ConsultaService service){
+    public ConsultaController(ConsultaService service) {
         this.service = service;
     }
 
     @GetMapping("/consultas")
-    public ResponseEntity<List<ConsultaDto>> listarConsultas() {
+    public ResponseEntity<Object> listarConsultas() {
         List<ConsultaDto> consultas = service.obterConsulta();
-        if (consultas.isEmpty()){
-            return ResponseEntity.status(404).build();
-        }
+        if (consultas.isEmpty()) return ResponseEntity.status(404).body("Lista Vazia de Consultas");
         return ResponseEntity.ok(consultas);
     }
 
-    @PostMapping("/consulta")
-    public ResponseEntity<String> cadastrarConsulta(@RequestBody ConsultaDto consultaDto) {
+    @PostMapping()
+    public ResponseEntity<String> cadastrarConsulta(@RequestBody @Valid ConsultaDto consultaDto) {
 
         service.inserirConsulta(consultaDto);
 
-        return ResponseEntity.ok().body("Consulta marcada com sucesso!");
+        return ResponseEntity.ok().body("Consulta inserida com sucesso");
 
     }
 
-    @PutMapping("/consultas/{id}")
-    public ResponseEntity<String> atualizarConsulta(@PathVariable long id, @RequestBody ConsultaDto consultaDto){
-        service.atualizarConsulta(id,consultaDto);
+    @PutMapping("/{id}")
+    public ResponseEntity<String> atualizarConsulta(@PathVariable @Valid long id, @RequestBody ConsultaDto consultaDto) {
+        service.atualizarConsulta(id, consultaDto);
 
         return ResponseEntity.ok().body("Consulta atualizada com sucesso");
     }
 
-    @DeleteMapping("/consulta/{id}")
-    public ResponseEntity<String> deletarConsulta(@PathVariable long id){
-        service.excluirConsulta(id);
-
-        return ResponseEntity.ok().body("Consulta cancelada com sucesso!");
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Object> deletarConsulta(@PathVariable long id) {
+        boolean resposta = service.excluirConsulta(id);
+        if (!resposta) return ResponseEntity.status(404).body("Consulta não encontrada!");
+        return ResponseEntity.ok("Consulta excluída com sucesso");
     }
 }
